e1f23794df5a047545ac9b44a14781deb5d11b75
ArtsAnton <artsanton@yandex.ru>
2021-07-25 21:22:54 +0300

    Changed DATABASES, ALLOWED_HOSTS. Setting up via the .env file.

--- project/settings.py
+++ project/settings.py
@@ -1,12 +1,17 @@
 import os
 
+from environs import Env
+
+
+env = Env()
+env.read_env()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': os.getenv("DB_ENGINE"),
         'HOST': os.getenv("DB_HOST"),
-        'PORT': '5434',
-        'NAME': 'checkpoint',
+        'PORT': os.getenv("DB_PORT"),
+        'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_USER_PASSWORD"),
     }
@@ -18,7 +23,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 ROOT_URLCONF = "project.urls"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -38,3 +43,5 @@ LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'Europe/Moscow'
 
 USE_TZ = True
+
+DEBUG = env.bool("DEBUG")
